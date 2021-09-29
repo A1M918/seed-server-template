30a09de6124f3274a92e5e265e0cff8da4f8205a
danyal <danyal@seed.im>
2021-09-29 18:17:16 +0500

    test

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(cors());
 console.log("asdasd");
 app.get('/', (req, res) => {
-    res.send("Working");
+    res.send("Working no");
 });
 app.listen(2500);
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
-app.use(cors())
-console.log("asdasd")
+app.use(cors());
 
-app.get('/', (req, res)=>{
-    res.send("Working")
-})
+app.get("/", (req, res) => {
+    res.send("Working no");
+});
 
 app.listen(2500);
