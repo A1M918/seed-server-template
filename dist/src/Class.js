12ef1b48605bf3a6fb48bffc5426a43300a1588e
danyal <danyal@seed.im>
2021-09-30 13:19:40 +0500

    test

--- dist/src/Class.js
+++ dist/src/Class.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Class = void 0;
 class Class {
     constructor() {
-        this.id = 110;
+        this.id = 10;
     }
     methodMultiply(no) {
         return no * this.id;
